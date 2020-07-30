0e015e7fc468c77a2cc6d3a075d10fae8f173556
petrubear <edison.martinez@gmail.com>
2020-07-29 19:08:59 -0500

    feat: load vets from service

--- pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,29 @@
 package emg.springframework.sfgpetclinic.controllers;
 
+import emg.springframework.sfgpetclinic.model.Vet;
+import emg.springframework.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping( {"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        Set<Vet> vets = vetService.findAll();
+        model.addAttribute("vets", vets);
         return "vets/index";
     }
 }
