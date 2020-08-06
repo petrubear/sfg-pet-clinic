e06676ed725f5c6762d6e0adae43d9f9176f4bf8
petrubear <edison.martinez@gmail.com>
2020-08-06 17:35:34 -0500

    feat: add PetTypeService

---
+++ pet-clinic-data/src/main/java/emg/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package emg.springframework.sfgpetclinic.services;
+
+import emg.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudServices<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/emg/springframework/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package emg.springframework.sfgpetclinic.services.map;
+
+import emg.springframework.sfgpetclinic.model.PetType;
+import emg.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType element) {
+        super.delete(element);
+    }
+
+    @Override
+    public PetType save(PetType element) {
+        return super.save(element);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
