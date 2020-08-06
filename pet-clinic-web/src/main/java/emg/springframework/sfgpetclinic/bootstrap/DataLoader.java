629f2c31f07ba3e48092fd0a2ba71ba4239a0e48
petrubear <edison.martinez@gmail.com>
2020-08-06 17:44:08 -0500

    feat: add pettype bootstrap data

--- pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package emg.springframework.sfgpetclinic.bootstrap;
 
 import emg.springframework.sfgpetclinic.model.Owner;
+import emg.springframework.sfgpetclinic.model.PetType;
 import emg.springframework.sfgpetclinic.model.Vet;
 import emg.springframework.sfgpetclinic.services.OwnerService;
+import emg.springframework.sfgpetclinic.services.PetTypeService;
 import emg.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,43 +13,52 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements CommandLineRunner {
 
-	private final OwnerService ownerService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+    private final VetService vetService;
 
-	private final VetService vetService;
+    @Autowired
+    public DataLoader(OwnerService ownerService, PetTypeService petTypeService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+        this.vetService = vetService;
+    }
 
-	@Autowired
-	public DataLoader(OwnerService ownerService, VetService vetService) {
-		this.ownerService = ownerService;
-		this.vetService = vetService;
-	}
+    @Override
+    public void run(String... args) throws Exception {
 
-	@Override
-	public void run(String... args) throws Exception {
-		var owner1 = new Owner();
-		owner1.setFirstName("Michael");
-		owner1.setLastName("Weston");
+        var dog = new PetType();
+        dog.setName("Dog");
+        var dogType = petTypeService.save(dog);
+        var cat = new PetType();
+        cat.setName("Cat");
+        var catType = petTypeService.save(cat);
 
-		var owner2 = new Owner();
-		owner2.setFirstName("Fiona");
-		owner2.setLastName("Glennane");
+        var owner1 = new Owner();
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
 
-		ownerService.save(owner1);
-		ownerService.save(owner2);
+        var owner2 = new Owner();
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glennane");
 
-		System.out.println("loaded owners...");
+        ownerService.save(owner1);
+        ownerService.save(owner2);
 
-		var vet1 = new Vet();
-		vet1.setFirstName("Sam");
-		vet1.setLastName("Axe");
+        System.out.println("loaded owners...");
 
-		var vet2 = new Vet();
-		vet2.setFirstName("Jessie");
-		vet2.setLastName("Porter");
+        var vet1 = new Vet();
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
 
-		vetService.save(vet1);
-		vetService.save(vet2);
+        var vet2 = new Vet();
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
 
-		System.out.println("loaded vets...");
-	}
+        vetService.save(vet1);
+        vetService.save(vet2);
+
+        System.out.println("loaded vets...");
+    }
 
 }
