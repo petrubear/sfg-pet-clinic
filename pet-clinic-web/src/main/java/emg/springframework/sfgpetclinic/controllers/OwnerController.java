febb95458480b6cafe997084dc47346bdddb75d5
petrubear <edison.martinez@gmail.com>
2020-08-25 18:08:21 -0500

    feat: add owner details page

--- pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -5,7 +5,10 @@ import emg.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -30,6 +33,14 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwner() {
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
     }
 
 }
