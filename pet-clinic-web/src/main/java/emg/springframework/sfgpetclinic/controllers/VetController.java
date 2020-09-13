ed946cdd53156ea91d9a90ba04dc495dab6572d5
petrubear <edison.martinez@gmail.com>
2020-09-13 11:05:35 -0500

    feat: add vets json endpoint

--- pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/emg/springframework/sfgpetclinic/controllers/VetController.java
@@ -5,7 +5,8 @@ import emg.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
@@ -19,11 +20,17 @@ public class VetController {
 		this.vetService = vetService;
 	}
 
-	@RequestMapping({ "/vets", "/vets/index", "/vets/index.html", "/vets.html" })
+	@GetMapping( {"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
 	public String listVets(Model model) {
 		Set<Vet> vets = vetService.findAll();
 		model.addAttribute("vets", vets);
 		return "vets/index";
+	}
+
+	@GetMapping("/api/vets")
+	public @ResponseBody
+	Set<Vet> getVetsJson() {
+		return vetService.findAll();
 	}
 
 }
